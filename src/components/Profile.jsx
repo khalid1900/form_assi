f7039ed6fec00e1a53951999f7d426b8b5380f03
khalid1900 <116065729+khalid1900@users.noreply.github.com>
2023-04-25 10:48:30 +0530

    third commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import styles from "../style/style";
-import { FormControlLabel, IconButton } from "@mui/material";
-import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import {
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  Radio,
+  RadioGroup,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+} from "@mui/material";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import {
   Box,
   Button,
   Dialog,
-  
   FormLabel,
-
   TextField,
   Typography,
 } from "@mui/material";
@@ -23,31 +31,37 @@ const Profile = () => {
     properties: "",
     address: "",
     mobile: "",
-    email:"",
-    photo:"",
-    gender:""
+    email: "",
+    photo: "",
+    gender: "",
+    isAgree: false,
   });
 
   const handleChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
+    let name = e.target.name;
+    let value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+    setUser({ ...user, [name]: value });
   };
 
   //   const navigate=useNavigate()
 
   const handleSave = () => {
-    setList([...list, user]);
+    setList([user]);
     console.log(list);
-    setOpen(false)
+    setOpen(false);
+
     // navigate("/displayProfile",{state:list})
   };
 
   const handleOpen = () => {
-      setOpen(true);
+    setOpen(true);
   };
 
-  const handleClose=()=>{
-    setList(false)
-  }
+  const handleClose = () => {
+    setList(false);
+  };
 
   return (
     <Box style={styles.main_wrapper}>
@@ -56,114 +70,206 @@ const Profile = () => {
         + Add Profile
       </Button>
 
-      <Dialog  open={open} onClose={handleClose}>
-        <div style={{width:"500px",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+      <Dialog open={open} onClose={handleClose}>
+        <div
+          style={styles.model}
+        >
+          <Typography variant="h3">Profile</Typography>
 
-        <IconButton color="primary" aria-label="upload picture" component="label">
-        <FormLabel>image</FormLabel>
-        {/* <input hidden accept="image/*" value={user.photo} onChange={(e)=>handleChange(setUser(e.target.files[0]))} type="file" /> */}
-        <PhotoCamera />
-      </IconButton>
+          {/* <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="label"
+          >
+            <FormLabel>image</FormLabel>
+            <input
+              hidden
+              accept="image/*"
+              value={user.photo}
+              onChange={(e) => handleChange(setUser(e.target.files[0]))}
+              type="file"
+            />
+            <PhotoCamera />
+          </IconButton> */}
 
-        
-        <Box sx={{ width: "80%", marginTop: "15px" }}>
-          <FormLabel>Store name: </FormLabel>
-          <TextField
-            onChange={(e) => {
-              handleChange(e);
-            }}
-            name="storeName"
-            size="small"
-            fullWidth
+          <input
+            type="file"
+            accept="/image/*"
+            onChange={(e) => setUser(e.target.files[0])}
           />
-        </Box>
-        <Box sx={{ width: "80%", marginTop: "15px" }}>
-          <FormLabel>Proprietor : </FormLabel>
-          <TextField
-            onChange={(e) => {
-              handleChange(e);
-            }}
-            name="properties"
-            size="small"
-            fullWidth
-          />
-        </Box>
 
-        <Box fullWidth >
+          <Box
+            fullWidth
+            style={styles.input_container}
+          >
+            <FormLabel>Store name: </FormLabel>
+            <TextField
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="storeName"
+              size="small"
+              style={styles.input_field}
+            />
+          </Box>
+          <Box
+            style={styles.input_container}
+          >
+            <FormLabel>Address : </FormLabel>
+            <TextField
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="address"
+              size="small"
+              style={styles.input_field}
+            />
+          </Box>
+
+          {/* <Box sx={{display:"flex",justifyContent:"space-around",alignItems:"center",width:"50%"}} >
             <FormLabel>Male</FormLabel>
-        <TextField  type="radio"
-        name="male"
-        checked={ user.gender=== 'male'}
+        <Radio  
+        name="gender"
+        value='male'
+        
         onChange={handleChange}/>
 
         <FormLabel>Female</FormLabel>
-        <TextField  type="radio"
-        name="female"
-        checked={ user.gender=== 'Female'}
+        <Radio  
+        name="gender"
+        value="female"
+   
         onChange={handleChange}/>
-        </Box>
+        </Box> */}
 
+          <Box style={styles.input_container} >
+            <FormLabel>Properioter : </FormLabel>
 
+            <TextField
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="properties"
+              size="small"
+              style={styles.small_input_field}
+              />
 
+            <TextField
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="properties"
+              size="small"
+              style={styles.small_input_field}
+              
+            />
+          </Box>
 
-        <Box sx={{ width: "80%", marginTop: "15px" }}>
-          <FormLabel>Address Name : </FormLabel>
-          <TextField
-            onChange={(e) => {
-              handleChange(e);
-            }}
-            name="address"
-            size="small"
-            fullWidth
-          />
-        </Box>
-        <Box sx={{ width: "80%", marginTop: "15px" }}>
-          <FormLabel>Email : </FormLabel>
-          <TextField
-            onChange={(e) => {
-              handleChange(e);
-            }}
-            name="email"
-            size="small"
-            fullWidth
-          />
-        </Box>
-        <Box sx={{ width: "80%", marginTop: "15px" }}>
-          <FormLabel>Mobile : </FormLabel>
-          <TextField
-            onChange={(e) => {
-              handleChange(e);
-            }}
-            name="mobile"
-            size="small"
-            fullWidth
-          />
-        </Box>
-        <Box  sx={{width:"250px", margin: "10px",display:"flex",justifyContent:"space-between" }}>
-          <Button onClick={() => handleSave()} variant="contained">
-            Save
-          </Button>
-          <Button onClick={() => setOpen(false)} variant="contained">
-            Cancel
-          </Button>
-        </Box>
+          <Box style={styles.input_container} >
+            <FormLabel>Gender : </FormLabel>
+            <FormControlLabel
+              onChange={(e) => handleChange(e)}
+              name="gender"
+              value="female"
+              control={<Radio />}
+              label="Female"
+            />
+            <FormControlLabel
+              onChange={(e) => handleChange(e)}
+              name="gender"
+              value="male"
+              control={<Radio />}
+              label="Male"
+            />
+          </Box>
+
+          <Box style={styles.input_container} >
+            <FormLabel>Mobile No.: </FormLabel>
+
+            <TextField
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="mobile"
+              size="small"
+              style={styles.small_input_field}
+              />
+
+            <TextField
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="mobile"
+              size="small"
+              style={styles.small_input_field}
+            />
+          </Box>
+
+          <Box style={styles.input_container} >
+            <FormLabel>Email : </FormLabel>
+            <TextField
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              name="email"
+              size="small"
+              style={styles.input_field}
+            />
+          </Box>
+
+          <Box sx={{  }}>
+            <Checkbox
+              checked={user.isAgree}
+              name="isAgree"
+              onChange={(e) => handleChange(e)}
+              inputProps={{ "aria-label": "controlled" }}
+            />
+            <Typography>I am Agree all terms & conditins</Typography>
+          </Box>
+
+          <Box style={styles.button_container} >
+            <Button
+              sx={{ bgcolor: "orange" }}
+              onClick={() => handleSave()}
+              variant="contained"
+            >
+              Save
+            </Button>
+            <Button
+              sx={{ bgcolor: "orange" }}
+              onClick={() => setOpen(false)}
+              variant="contained"
+            >
+              Cancel
+            </Button>
+          </Box>
         </div>
       </Dialog>
 
-            {list.map((item,index)=>{
-                return(
-                    <>
-                    {/* <img src={URL.createObjectURL(item.photo)} alt='pic'/> */}
-                    <p>{item.storeName}</p>
-                    <p>{item.properties}</p>
-                    <p>{item.email}</p>
-                    <p>{item.address}</p>
-                    <p>{item.mobile}</p>
-                    <p>{item.gender.value}</p>
-                    </>
-                )
-            })}
-
+      <Table style={styles.react_table} >
+        <TableHead>
+          <TableCell>Stor Name</TableCell>
+          <TableCell>Adress</TableCell>
+          <TableCell>Proprioter</TableCell>
+          <TableCell>Email</TableCell>
+          <TableCell>Mobile No</TableCell>
+          <TableCell>Gender</TableCell>
+        </TableHead>
+        <TableBody>
+          {list.map((item, index) => {
+            return (
+              <>
+                <TableCell key={index}>{item.storeName}</TableCell>
+                <TableCell>{item.address}</TableCell>
+                <TableCell>{item.properties}</TableCell>
+                <TableCell> {item.email}</TableCell>
+                <TableCell>{item.mobile}</TableCell>
+                <TableCell>{item.gender}</TableCell>
+              </>
+            );
+          })}
+        </TableBody>
+      </Table>
     </Box>
   );
 };
--- src/style/style.js
+++ src/style/style.js
@@ -1,13 +1,52 @@
-const styles={
-main_wrapper:{
-    // border:"1px solid black",
-    width:"100%",
-    height:"100%",
-    display:"flex",
-    flexDirection:"column",
-    alignItems:"center",
-    justifyContent:"center",
-}
-}
-
-export default styles;
+const styles = {
+    main_wrapper: {
+      // border:"1px solid black",
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    model: {
+      width: "600px",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    input_container: {
+      width: "80%",
+      marginTop: "15px",
+      display: "flex",
+      justifyContent: "space-between",
+      backgroundcolor: "red",
+    },
+    input_field: {
+      width: "70%",
+      backgroundcolor: "#D8EDE5",
+      border: "none",
+    },
+    small_input_field: {
+      width: "30%",
+      bgcolor: "#D8EDE5",
+      border: "none",
+    },
+    check_box_container: {
+      display: "flex",
+      alignItems: "center",
+    },
+    button_container: {
+      width: "250px",
+      margin: "10px",
+      display: "flex",
+      justifyContent: "space-between",
+    },
+    react_table: {
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+    },
+  };
+  
+  export default styles;
